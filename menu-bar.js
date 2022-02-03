c8de2519a8f6058dd9488d30d318165435f1b556
Maximilian Scheiber <maximilian.scheiber98@gmail.com>
2022-02-03 16:36:08 +0100

    integrated basic vue and tailwind

---
+++ menu-bar.js
@@ -0,0 +1,10 @@
+app.component("menu-bar", {
+    template:
+        /*html*/
+        `
+        <div class="flex flex-row">
+        <button type="button" class="border-8">Button 1</button>
+        <button type="button" class="border-8">Button 2</button>
+        </div>
+        `
+})
