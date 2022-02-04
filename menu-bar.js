c1bb8a73f958496463df5cc5abda4fa2697935f6
Maximilian Scheiber <maximilian.scheiber98@gmail.com>
2022-02-04 17:32:44 +0100

    update menubar

--- menu-bar.js
+++ menu-bar.js
@@ -2,9 +2,13 @@ app.component("menu-bar", {
     template:
         /*html*/
         `
-        <div class="flex flex-row">
-        <button type="button" class="border-8">Button 1</button>
-        <button type="button" class="border-8">Button 2</button>
+        <nav>
+        <div class="flex justify-around bg-gray-500">
+        <a href="#" class="bg-gray-200 p-4 m-4 border-4 border-black">Ueber mich</a>
+        <a href="#" class="bg-gray-200 p-4">Wuerfel</a>
+        <a href="#" class="bg-gray-200 p-4">Login</a>
+        <a href="#" class="bg-gray-200 p-4">SignUp</a>
         </div>
+        </nav>
         `
 })
