5feaf7788245b55217687304152b07c7c94c55e0
Maximilian Scheiber <maximilian.scheiber98mail.com>
2022-02-07 10:35:37 +0100

    playing around

--- menu-bar.js
+++ menu-bar.js
@@ -2,13 +2,19 @@ app.component("menu-bar", {
     template:
         /*html*/
         `
-        <nav>
-        <div class="flex justify-around bg-gray-500">
-        <a href="#" class="bg-gray-200 p-4 m-4 border-4 border-black">Ueber mich</a>
-        <a href="#" class="bg-gray-200 p-4">Wuerfel</a>
-        <a href="#" class="bg-gray-200 p-4">Login</a>
-        <a href="#" class="bg-gray-200 p-4">SignUp</a>
+     <nav>
+        <div class="font-mono max-w-7xl mx-auto my-6">
+            <div class="flex justify-around">
+                <div class="space-x-4">
+                    <a href="./about-me.html" class="bg-gray-200 p-4 border-4 border-black">Über mich</a>
+                    <a href="./cube.html" class="bg-gray-200 p-4 border-4 border-black">Würfel</a>
+                </div>
+                <div class="space-x-4">
+                    <a href="#" class="bg-gray-200 p-4 border-4 border-black">Login</a>
+                    <a href="#" class="bg-gray-200 p-4 border-4 border-black">SignUp</a>
+                </div>
+            </div>
         </div>
-        </nav>
+    </nav>
         `
 })
