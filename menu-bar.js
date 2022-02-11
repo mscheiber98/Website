42bf5c82fd2104ec17464b6236a0546a07b78822
Maximilian Scheiber <maximilian.scheiber98@gmail.com>
2022-02-11 15:28:40 +0100

    small changes

--- menu-bar.js
+++ menu-bar.js
@@ -2,8 +2,8 @@ app.component("menu-bar", {
     template:
         /*html*/
         `
-     <nav>
-        <div class="font-mono max-w-7xl mx-auto my-6">
+     <nav class="sticky top-0">
+        <div class="font-mono mx-auto py-6 border-4 border-black bg-purple-400">
             <div class="flex justify-around">
                 <div class="space-x-4">
                     <a href="./about-me.html" class="bg-gray-200 p-4 border-4 border-black">Über mich</a>
